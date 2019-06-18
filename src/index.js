484846549dbedc27c5beec6c119cbf2b486d7686
Henry <henrygraves95@gmail.com>
2019-06-18 01:16:36 -0700

    completed react components tutorial, this is cool

--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import CommentDetail from './CommentDetail';
+import fake from 'faker';
+import ApprovalCard from './ApprovalCard';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// We learned about props here too!
+  
+const App = () => {
+  return (
+    <div className="ui container comments">
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+      <ApprovalCard>
+       <CommentDetail 
+          author="sam" 
+          timeAgo="today at 6pm"
+          comment="nice"
+          avatar={fake.image.avatar()}
+        />
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail 
+          author="no u" 
+          timeAgo="today at 7pm"
+          comment="oof"
+          avatar={fake.image.avatar()}
+        />
+      </ApprovalCard>
+      
+    </div>
+  );
+};
+
+ReactDOM.render(<App />,
+  document.querySelector('#root'));
